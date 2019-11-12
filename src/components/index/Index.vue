2b9c8b617f78632939cb28bff64083b499e7d3d8
luyang <654922729@qq.com>
2019-11-12 17:16:04 +0800

    no message

---
+++ src/components/index/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-input v-model="user_name" placeholder="用户名"></el-input>
+    <el-input v-model="pass_word" placeholder="密码"></el-input>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      user_name: '',
+      pass_word: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
